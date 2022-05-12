7c4d0e08eccf0b03c6074595e4427ae4e3703861
Ryo Nakamura <r7kamura@gmail.com>
2022-05-13 06:32:06 +0900

    Set GITHUB_ACCESS_TOKEN

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ echo /vendor/ >> .git/info/exclude
 
 git config remote.origin.url "git@github.com:${GITHUB_REPOSITORY}.git"
 
+export GITHUB_ACCESS_TOKEN="$2"
+
 bundle exec rubocop_challenger go \
   --base-branch="$1" \
   --email="${GITHUB_ACTOR}@users.noreply.github.com" \
